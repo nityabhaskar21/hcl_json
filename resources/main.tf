9df7e96c121a127d51db94e0fb48a98d2eec1ca8
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-05-13 13:03:06 +0530

    Modified regex and added map variable in  main.tf

--- resources/main.tf
+++ resources/main.tf
@@ -24,3 +24,21 @@ variable "docker_ports" {
     }
   ]
 }
+
+variable "key" {
+  type = "string"
+}
+
+variable "images" {
+  type = "map"
+
+  default = {
+    us-east-1 = "image-1234"
+    us-west-2 = "image-4567"
+  }
+}
+
+variable "zones" {
+  type = "list"
+  default = ["us-east-1a", "us-east-1b"]
+}

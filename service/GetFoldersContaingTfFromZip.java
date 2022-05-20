3ae5afeb84512a3a313baf5d3578f177b839f4f4
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-05-20 09:35:00 +0530

    Renamed the file

---
+++ service/GetFoldersContaingTfFromZip.java
@@ -0,0 +1,36 @@
+package service;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class GetFoldersContaingTfFromZip {
+
+    public static void main(String[] args) throws IOException {
+        ZipFile zip = null;
+		try {
+			zip = new ZipFile("E:\\hcl_json\\resources\\test.zip");
+            Set<String> folderSet = new HashSet<> ();
+			for (Enumeration<?> e = zip.entries(); e.hasMoreElements();) {
+				ZipEntry entry = (ZipEntry) e.nextElement();
+				if (!entry.isDirectory()) {
+                    if(List.of(".tf", ".json", ".tf.json").stream().anyMatch(ext -> entry.getName().toLowerCase().endsWith(ext))) {
+                        folderSet.add(entry.getName().replaceFirst("(?<=[\\/\\\\])([\\w\\.\\s\\%]+)$", ""));
+                    }
+					
+				}
+                
+			}
+            System.out.println(folderSet);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			zip.close();
+		}
+    }
+    
+}

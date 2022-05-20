5731a4a5f8f5ff6668a3d96eb6355bf4c508d755
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-05-20 15:22:44 +0530

    Modified regex. Added "=" validation in value

--- resources/folder/provider.tf
+++ resources/folder/provider.tf
@@ -1,9 +1,20 @@
-provider "aws" {
-  version = "3.3.0"
+provider "aws_test" {
+    # region = "ap-south-1"
+    version = "= 3.3.0"
 }
 
-provider "google" {
-  alias = "google-dev"
-  project = "acme-app"
-  region  = "us-central1"
+provider "template" {
+    version = "= 2.1.2"
+}
+
+provider "vault" {
+    version = "= 2.12.2"
+}
+
+terraform {
+  backend "consul" {
+    address = "localhost"
+    scheme  = "http"
+    path    = "cmp/tf/states/ca1/ops"
+  }
 }

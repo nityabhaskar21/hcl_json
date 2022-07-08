5a4d3dac68d3d179c36726fdc8ea4db9f8ce05b8
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-07-08 15:44:15 +0530

    Updated regex pattern.

--- resources/folder1/providers.tf
+++ resources/folder1/providers.tf
@@ -9,3 +9,11 @@ provider "google" {
 }
 
 provider "oci" {}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.current_user_ocid
+  # fingerprint      = var.fingerprint
+  # private_key_path = var.private_key_path
+  region           = var.region
+}

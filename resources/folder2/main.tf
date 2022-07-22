2b26aa5d3b756d392a41b843bcf2da41d082692c
Nityanand Bhaskar <nityanand.shaktidharshiv@sifycorp.com>
2022-07-22 17:12:50 +0530

    Updated regex

--- resources/folder2/main.tf
+++ resources/folder2/main.tf
@@ -21,11 +21,17 @@ variable "docker_ports" {
   }))
   default = [
     {
-      internal = 8300
+      internal = 8300 
       external = 8300
       protocol = "tcp"
     }
   ]
+}
+
+variable "public_subnet_cidr_blocks" {
+description = "Available cidr blocks for public subnets."
+type = list(string)
+default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
 }
 
 
